3c40d772daf5df2c672d1effc713f64a57585926
oneHs <keepcx@keepcx.com>
2019-05-15 10:48:02 +0800

    两个变量值交换不用第三个变量

--- src/helloword.java
+++ src/helloword.java
@@ -1,6 +1,14 @@
+
 public class helloword {
+    //两个变量值交换
     public static void main(String[] args) {
-        System.out.println("s,ssord");
-        System.out.println("sss");
+        int a=10;
+        int b=20;
+        System.out.println("交换前：a="+a+"，b="+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("交换后：a="+a+"，b="+b);
+
     }
 }
